c4ea079e9e45ddc680eb64f31f5e525126958ed9
Gaurav Thawait <iamgauravthawait@gmail.com>
2025-03-29 17:53:23 +0530

    vercel.json again fixed

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ require('module-alias/register');
 const mongoose = require('mongoose');
 const { globSync } = require('glob');
 const path = require('path');
-const { sequelize, connectDB } = require('../config/connectDB.js');
+const { sequelize, connectDB } = require('../config/connectDB');
 
 // Make sure we are running node 7.6+
 const [major, minor] = process.versions.node.split('.').map(parseFloat);
